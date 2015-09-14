853bd9d7e3bd20e5fe43cdbc0ff026afc9c3acb5
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-14 02:57:28 +0000

    Created tag 1.4.2.
    git-svn-id: https://plugins.svn.wordpress.org/wp-email-template/tags/1.4.2@1244752 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/less/compile_less_sass_class.php
+++ admin/less/compile_less_sass_class.php
@@ -1,7 +1,7 @@
 <?php
 class Compile_Less_Sass {
 
-	public function Compile_Less_Sass(){
+	public function __construct(){
 		$this->init();
 	}
 	public function init(){
@@ -20,23 +20,27 @@ class Compile_Less_Sass {
 		// Write less file
     	if ( is_writable( $css_file ) && is_writable( $css_min_file ) ) {
 
-			if ( ! class_exists( 'lessc' ) ){
+			if ( ! class_exists( 'a3_lessc' ) ){
 				include( dirname( __FILE__ ) . '/lib/lessc.inc.php' );
 			}
-			if ( ! class_exists( 'cssmin' ) ){
+			if ( ! class_exists( 'a3_CSSmin' ) ){
 				include( dirname( __FILE__ ) . '/lib/cssmin.inc.php' );
 			}
 
 			try {
 
-				$less         = new lessc;
+				$less         = new a3_lessc();
 
 				$compiled_css = $less->compileFile( $less_file );
 
 				if ( $compiled_css != '' ){
 					$wp_filesystem->put_contents( $css_file, $compiled_css );
 
-					$compiled_css_min = CssMin::minify( $compiled_css );
+					$compressor = new a3_CSSmin();
+					$compressor->set_memory_limit( '512M' );
+					$compressor->set_max_execution_time( 120 );
+
+					$compiled_css_min = $compressor->run( $compiled_css );
 					if ( $compiled_css_min != '' )
 						$wp_filesystem->put_contents( $css_min_file, $compiled_css_min );
 				}
